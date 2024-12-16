4c0e2e25da7dd12280ee4a7856f2bd75d8ad8f02
csierra <csierra@bgc-jena.mpg.de>
2024-12-16 16:27:22 +0100

    Changed plot function

---
+++ R/plotGas.R
@@ -0,0 +1,18 @@
+#' Plot data for a specific gas species
+#' 
+#' @param x release data.frame
+#' @param gas character string with the name of the gas to plot
+#' @param start character string with the start date for the horizonta axis
+#' @param end character string with the end date for the horizontal axis
+#' @param sigma character string with the name of the variable that represents the uncertainty in the data
+#' @param type argument passed to plot() for the type of plot
+#' @param ... other arguments passed to plot()
+#' 
+#' @return a plot
+#' @export
+
+plotGas<-function(x, gas, start="2021-06-01", end="2023-12-31", sigma=NULL, type="b", ...){
+  tm<-as.POSIXct(x$endTime, tz="UTC") - (60*30) # Last 30 minutes
+  plot(tm, x[,gas],  pch=19, xlab="", type=type, xlim=c(as.POSIXct(start), as.POSIXct(end)), bty="n", ...)
+  if(!is.null(sigma)) arrows(x0=tm, y0=x[,gas]-x[,sigma], y1=x[,gas]+x[,sigma], code=3, length=0.05, angle=90, lwd=0.5)
+}
