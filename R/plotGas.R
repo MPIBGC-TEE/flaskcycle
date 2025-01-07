ef241ae35c01a13bcafd1e5ad6dcc7be407b6ff3
csierra <csierra@bgc-jena.mpg.de>
2025-01-07 15:51:41 +0100

    Added option to plot flagged observations

--- R/plotGas.R
+++ R/plotGas.R
@@ -6,13 +6,15 @@
 #' @param end character string with the end date for the horizontal axis
 #' @param sigma character string with the name of the variable that represents the uncertainty in the data
 #' @param type argument passed to plot() for the type of plot
+#' @param flag vector of integers with observations to flag
 #' @param ... other arguments passed to plot()
 #' 
 #' @return a plot
 #' @export
 
-plotGas<-function(x, gas, start="2021-06-01", end="2023-12-31", sigma=NULL, type="b", ...){
+plotGas<-function(x, gas, start="2021-06-01", end="2023-12-31", sigma=NULL, type="b", flag=NULL, ...){
   tm<-as.POSIXct(x$endTime, tz="UTC") - (60*30) # Last 30 minutes
   plot(tm, x[,gas],  pch=19, xlab="", type=type, xlim=c(as.POSIXct(start), as.POSIXct(end)), bty="n", ...)
   if(!is.null(sigma)) arrows(x0=tm, y0=x[,gas]-x[,sigma], y1=x[,gas]+x[,sigma], code=3, length=0.05, angle=90, lwd=0.5)
+  if(!is.null(flag)) points(tm[flag], x[flag, gas], col=2, pch=19)
 }
--- scripts/build_from_source.sh
+++ scripts/build_from_source.sh
@@ -1,1 +1,2 @@
+Rscript buildRd.R
 R CMD build ../
